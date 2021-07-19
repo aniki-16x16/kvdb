4bdef6f615960206677adc67dc52b45a6522bb26
aniki-16x16 <aniki_16x16@outlook.com>
2021-07-19 12:14:54 +0800

    feat: set,get命令

---
+++ src/core.rs
@@ -0,0 +1,127 @@
+use serde::{Deserialize, Serialize};
+use std::{
+    collections::HashMap,
+    fs::{File, OpenOptions},
+    io::{self, BufReader, BufWriter, Read, Seek, SeekFrom, Write},
+    path::Path,
+};
+
+#[derive(Debug, Serialize, Deserialize)]
+enum Command {
+    Set(String, String),
+    Remove(String),
+}
+
+struct CommandInfo {
+    pos: u64,
+    len: u64,
+}
+
+pub struct KVDB {
+    writer: BufWriterWithPos<File>,
+    reader: BufReaderWithPos<File>,
+    index: HashMap<String, CommandInfo>,
+}
+
+impl KVDB {
+    pub fn new(path: &str) -> io::Result<Self> {
+        if !Path::new(path).exists() {
+            File::create(path)?;
+        }
+        Ok(KVDB {
+            writer: BufWriterWithPos::new(OpenOptions::new().append(true).open(path)?)?,
+            reader: BufReaderWithPos::new(OpenOptions::new().read(true).open(path)?)?,
+            index: HashMap::new(),
+        })
+    }
+
+    pub fn set(&mut self, k: String, v: impl Serialize) -> io::Result<()> {
+        let pos = self.writer.pos;
+        let cmd = Command::Set(k.clone(), serde_json::to_string(&v).unwrap());
+        serde_json::to_writer(&mut self.writer, &cmd)?;
+        self.writer.flush()?;
+        self.index.insert(
+            k,
+            CommandInfo {
+                pos,
+                len: self.writer.pos - pos,
+            },
+        );
+        Ok(())
+    }
+
+    pub fn get(&mut self, k: String) -> io::Result<Option<String>> {
+        match self.index.get(&k) {
+            None => Ok(None),
+            Some(cmd_info) => {
+                let reader = &mut self.reader;
+                reader.seek(SeekFrom::Start(cmd_info.pos))?;
+                let cmd_reader = reader.take(cmd_info.len);
+                if let Command::Set(_, v) = serde_json::from_reader(cmd_reader)? {
+                    Ok(Some(v))
+                } else {
+                    Err(io::Error::new(
+                        io::ErrorKind::Other,
+                        "Serde from_reader() failed",
+                    ))
+                }
+            }
+        }
+    }
+}
+
+#[derive(Debug)]
+struct BufWriterWithPos<W: Write + Seek> {
+    writer: BufWriter<W>,
+    pos: u64,
+}
+
+impl<W: Write + Seek> BufWriterWithPos<W> {
+    fn new(mut inner: W) -> io::Result<Self> {
+        Ok(BufWriterWithPos {
+            pos: inner.seek(SeekFrom::End(0))?,
+            writer: BufWriter::new(inner),
+        })
+    }
+}
+
+impl<W: Write + Seek> Write for BufWriterWithPos<W> {
+    fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
+        let len = self.writer.write(buf)?;
+        self.pos += len as u64;
+        Ok(len)
+    }
+
+    fn flush(&mut self) -> io::Result<()> {
+        self.writer.flush()
+    }
+}
+
+struct BufReaderWithPos<W: Read + Seek> {
+    reader: BufReader<W>,
+    pos: u64,
+}
+
+impl<W: Read + Seek> BufReaderWithPos<W> {
+    fn new(mut inner: W) -> io::Result<Self> {
+        Ok(BufReaderWithPos {
+            pos: inner.seek(SeekFrom::Current(0))?,
+            reader: BufReader::new(inner),
+        })
+    }
+}
+
+impl<W: Read + Seek> Read for BufReaderWithPos<W> {
+    fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
+        let len = self.reader.read(buf)?;
+        self.pos += len as u64;
+        Ok(len)
+    }
+}
+
+impl<W: Read + Seek> Seek for BufReaderWithPos<W> {
+    fn seek(&mut self, pos: SeekFrom) -> io::Result<u64> {
+        self.pos = self.reader.seek(pos)?;
+        Ok(self.pos)
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,43 @@
+use serde::{Deserialize, Serialize};
+use std::io;
+
+use crate::core::KVDB;
+
+mod core;
+
+fn main() -> io::Result<()> {
+    let mut db = KVDB::new("0.log")?;
+    db.set(String::from("kvdb"), true)?;
+    db.set(String::from("author"), "Aniki")?;
+    db.set(String::from("version"), 0)?;
+    db.set(String::from("version"), 1)?;
+    db.set(
+        String::from("user-1"),
+        Person {
+            name: String::from("Loren Lausi"),
+            age: 15,
+        },
+    )?;
+    db.set(
+        String::from("user-2"),
+        Person {
+            name: String::from("Daiv Oiqud"),
+            age: 99,
+        },
+    )?;
+    db.set(
+        String::from("user-1"),
+        Person {
+            name: String::from("Sadkuren Zoack"),
+            age: 1,
+        },
+    )?;
+    println!("{:#?}", db.get(String::from("user-1"))?.unwrap());
+    Ok(())
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Person {
+    name: String,
+    age: usize,
+}
