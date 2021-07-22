cf26d6c36c440864943469752dc8c431cfed1700
aniki-16x16 <aniki_16x16@outlook.com>
2021-07-22 11:18:04 +0800

    feat: 日志分片与压缩

--- src/core.rs
+++ src/core.rs
@@ -1,7 +1,7 @@
 use serde::{Deserialize, Serialize};
 use std::{
     collections::HashMap,
-    fs::{File, OpenOptions},
+    fs::{self, File, OpenOptions},
     io::{self, BufReader, BufWriter, Read, Seek, SeekFrom, Write},
     path::Path,
 };
@@ -16,32 +16,86 @@ enum Command {
 struct CommandInfo {
     pos: u64,
     len: u64,
+    log_idx: u64,
 }
 
 pub struct KVDB {
     writer: BufWriterWithPos<File>,
-    reader: BufReaderWithPos<File>,
+    /// 每个日志文件对应一个reader
+    readers: HashMap<u64, BufReaderWithPos<File>>,
     index: HashMap<String, CommandInfo>,
+    cur_log_idx: u64,
+    log_size: u64,
 }
 
 impl KVDB {
-    pub fn new(path: &str) -> io::Result<Self> {
-        if !Path::new(path).exists() {
-            File::create(path)?;
-        }
-        let mut db = KVDB {
-            writer: BufWriterWithPos::new(OpenOptions::new().append(true).open(path)?)?,
-            reader: BufReaderWithPos::new(OpenOptions::new().read(true).open(path)?)?,
-            index: HashMap::new(),
+    pub fn new() -> io::Result<Self> {
+        let mut reader = HashMap::new();
+        let mut logs = if !Path::new("logs/").is_dir() {
+            fs::create_dir("logs")?;
+            File::create("logs/0.log")?;
+            reader.insert(
+                0,
+                BufReaderWithPos::new(OpenOptions::new().read(true).open("logs/0.log")?)?,
+            );
+            vec![0u64]
+        } else {
+            // 遍历logs目录下所有的log文件，创建对应每个日志文件的reader
+            // 并返回日志序列数组
+            let mut tmp = vec![];
+            for entity in fs::read_dir("logs")? {
+                let entity = entity.unwrap();
+                if entity.metadata().unwrap().is_file() {
+                    let file_idx = entity
+                        .file_name()
+                        .to_str()
+                        .unwrap()
+                        .split('.')
+                        .collect::<Vec<_>>()[0]
+                        .parse::<u64>()
+                        .unwrap();
+
+                    reader.insert(
+                        file_idx,
+                        BufReaderWithPos::new(
+                            OpenOptions::new()
+                                .read(true)
+                                .open(build_log_path(file_idx))?,
+                        )?,
+                    );
+                    tmp.push(file_idx);
+                }
+            }
+            tmp
         };
-        db.start()?;
+        logs.sort();
+        // 写入最新的日志中
+        let cur_log_index = *logs.iter().max().unwrap();
+
+        let mut db = KVDB {
+            writer: BufWriterWithPos::new(
+                OpenOptions::new()
+                    .append(true)
+                    .open(build_log_path(cur_log_index))?,
+            )?,
+            readers: reader,
+            index: HashMap::new(),
+            cur_log_idx: cur_log_index,
+            log_size: 64 * 1024 * 1024, // 默认日志阈值，超过后压缩日志
+        };
+        println!("正在构建内存索引......");
+        for log_idx in logs {
+            db.load(log_idx)?;
+        }
+        println!("内存索引构建完成");
         Ok(db)
     }
 
-    pub fn start(&mut self) -> io::Result<()> {
-        println!("正在构建内存索引......");
-        let reader = &mut self.reader;
+    /// 根据日志内容构建索引
+    fn load(&mut self, log_idx: u64) -> io::Result<()> {
+        let reader = self.readers.get_mut(&log_idx).unwrap();
         let mut pos = reader.seek(SeekFrom::Start(0))?;
+
         let mut iter = serde_json::Deserializer::from_reader(reader).into_iter::<Command>();
         while let Some(cmd) = iter.next() {
             let new_pos = iter.byte_offset() as u64;
@@ -52,6 +106,7 @@ impl KVDB {
                         CommandInfo {
                             pos,
                             len: new_pos - pos,
+                            log_idx,
                         },
                     );
                 }
@@ -61,7 +116,6 @@ impl KVDB {
             }
             pos = new_pos;
         }
-        println!("内存索引构建完成");
         Ok(())
     }
 
@@ -75,8 +129,10 @@ impl KVDB {
             CommandInfo {
                 pos,
                 len: self.writer.pos - pos,
+                log_idx: self.cur_log_idx,
             },
         );
+        self.check_log_size()?;
         Ok(())
     }
 
@@ -84,10 +140,9 @@ impl KVDB {
         match self.index.get(&k) {
             None => Ok(None),
             Some(cmd_info) => {
-                let reader = &mut self.reader;
+                let reader = self.readers.get_mut(&cmd_info.log_idx).unwrap();
                 reader.seek(SeekFrom::Start(cmd_info.pos))?;
-                let cmd_reader = reader.take(cmd_info.len);
-                if let Command::Set(_, v) = serde_json::from_reader(cmd_reader)? {
+                if let Command::Set(_, v) = serde_json::from_reader(reader.take(cmd_info.len))? {
                     Ok(Some(v))
                 } else {
                     Err(io::Error::new(
@@ -104,8 +159,73 @@ impl KVDB {
         self.writer.writeln(cmd)?;
         self.writer.flush()?;
         self.index.remove(&k);
+        self.check_log_size()?;
         Ok(())
     }
+
+    /// 压缩日志文件，idx+1为压缩后的日志，idx+2为操作完成后的目标日志
+    /// 保存新文件的reader，同时删除老的日志文件与对应reader
+    fn compact(&mut self) -> io::Result<()> {
+        let old_idx = self.cur_log_idx;
+        self.cur_log_idx += 2;
+        for i in 1..=2 {
+            File::create(build_log_path(old_idx + i))?;
+            self.readers.insert(
+                old_idx + i,
+                BufReaderWithPos::new(
+                    OpenOptions::new()
+                        .read(true)
+                        .open(build_log_path(old_idx + i))?,
+                )?,
+            );
+        }
+        self.writer = BufWriterWithPos::new(
+            OpenOptions::new()
+                .append(true)
+                .open(build_log_path(self.cur_log_idx))?,
+        )?;
+
+        let mut writer = BufWriterWithPos::new(
+            OpenOptions::new()
+                .append(true)
+                .open(build_log_path(old_idx + 1))?,
+        )?;
+        for cmd_info in self.index.values_mut() {
+            let reader = self.readers.get_mut(&cmd_info.log_idx).unwrap();
+            reader.seek(SeekFrom::Start(cmd_info.pos))?;
+            io::copy(&mut reader.take(cmd_info.len), &mut writer)?;
+            *cmd_info = CommandInfo {
+                pos: writer.pos - cmd_info.len,
+                len: cmd_info.len,
+                log_idx: old_idx + 1,
+            };
+        }
+        writer.flush()?;
+        let stale_idxs = self
+            .readers
+            .keys()
+            .filter(|idx| **idx <= old_idx)
+            .cloned()
+            .collect::<Vec<_>>();
+        for idx in stale_idxs {
+            self.readers.remove(&idx);
+            fs::remove_file(build_log_path(idx))?;
+        }
+        Ok(())
+    }
+
+    fn check_log_size(&mut self) -> io::Result<()> {
+        if self.writer.pos >= self.log_size {
+            println!("开始压缩 {}.log", self.cur_log_idx);
+            self.compact()?;
+            println!("压缩完成并写入 {}.log", self.cur_log_idx - 1);
+        }
+        Ok(())
+    }
+}
+
+fn build_log_path(idx: u64) -> String {
+    format!("logs/{}.log", idx)
 }
 
 #[derive(Debug)]
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
-use std::io;
+use std::{
+    io,
+};
 
 use rand::Rng;
 
@@ -7,20 +9,27 @@ use crate::core::KVDB;
 mod core;
 
 fn main() -> io::Result<()> {
-    let mut db = KVDB::new("0.log")?;
+    let mut db = KVDB::new()?;
     let mut rng = rand::thread_rng();
-    let k = format!("user::{}", rng.gen_range(0..100));
-    if let Some(value) = db.get(k.clone())? {
-        println!("{} => {}", k, serde_json::from_str::<i32>(&value)?);
-    } else {
-        println!("key[{}] doesn't exist", k);
-    }
-    for _ in 0..50 {
-        let k = format!("user::{}", rng.gen_range(0..100));
-        if rng.gen::<f64>() < 0.2 {
-            db.remove(k)?;
-        } else {
-            db.set(k, rng.gen::<i32>())?;
+    for i in 0..20 {
+        println!("第{}轮", i + 1);
+        println!("随机写入10万条数据");
+        for _ in 0..100000 {
+            let k = format!("user::{}", rng.gen_range(0..1000));
+            if rng.gen::<f64>() < 0.2 {
+                db.remove(k)?;
+            } else {
+                db.set(k, rng.gen::<(i32, bool, f64)>())?;
+            }
+        }
+        println!("随机获取100条数据");
+        for _ in 0..100 {
+            let k = format!("user::{}", rng.gen_range(0..1000));
+            if let Some(value) = db.get(k.clone())? {
+                println!("[{}] => {:?}", k, serde_json::from_str::<(i32, bool, f64)>(&value)?);
+            } else {
+                println!("key [{}] 不存在", k);
+            }
         }
     }
     Ok(())
